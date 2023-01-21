d881a9a768c022430be1761547b405fe970c2bab
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-01-21 12:44:24 +0100

    add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
+import { Component } from "react"
+import Searchbar from './Searchbar/Searchbar'
+
+
+export class App extends Component {
+  render () {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Searchbar />
     </div>
   );
+    }
 };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react"
+
+export default class Searchbar extends Component {
+    render () {
+    return (
+<header className="Searchbar">
+  <form className="SearchForm">
+    <button type="submit" className="SearchForm-button">
+      <span className="SearchForm-button-label">Search</span>
+    </button>
+
+    <input
+      className="SearchForm-input"
+      type="text"
+      autoComplete="off"
+      autoFocus
+      placeholder="Search images and photos"
+    />
+  </form>
+</header>
+    )
+    }
+}
