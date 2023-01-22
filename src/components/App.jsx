d378c64b55122a11b8ee3de182a517a612a41cdc
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-01-23 00:56:09 +0100

    update app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,34 +11,43 @@ export class App extends Component {
     images: [],
     page: 1,
     isLoading: false,
+    totalImgs: 0,
   };
   componentDidUpdate(prevProps, prevState) {
     const { query, page } = this.state;
     if (prevState.query === query || prevState.page !== page) {
-      fetchImages(query, page).then(resp => {
-        this.setState(({ images }) => ({
-          images: page === 1 ? [...resp.hits] : [...images, ...resp.hits],
-        }));
-      })
-      .finally(() => this.setState({isLoading: false}));
+      fetchImages(query, page)
+        .then(resp => {
+          this.setState(({ images}) => ({
+            images: page === 1 ? [...resp.hits] : [...images, ...resp.hits],
+            totalImgs: resp.totalHits,
+          }));
+        })
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
     }
   }
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1, isLoading: true }));
+  };
 
   handleSubmit = query => {
     this.setState({ query, isLoading: true });
   };
-  handleLoadMore = () => {
-    this.setState((prevState) => ({ page: prevState.page + 1, isLoading: true }));
-  };
 
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Searchbar onSubmit={this.handleSubmit} />
         <ImageGallery images={this.state.images} />
 
-        {this.state.isLoading ? <Loader /> : !!this.state.images.length && <Button onClick={this.handleLoadMore} />}
-        
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (this.state.images.length !== 0 &&
+          this.state.images.length < this.state.totalImgs && <Button onClick={this.handleLoadMore} />
+        )}
       </div>
     );
   }
