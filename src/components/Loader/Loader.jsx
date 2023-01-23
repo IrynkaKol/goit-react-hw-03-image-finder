fbc0c36b932bbba273aab0e2a96c83ef47f750ce
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-01-23 10:54:06 +0100

    update loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,11 +4,15 @@ import { Circles } from 'react-loader-spinner';
 export const Loader = () => {
   return (
     <Circles
+      className="Loader"
       height="60"
       width="60"
       color="#4fa94d"
       ariaLabel="circles-loading"
-      wrapperStyle={{}}
+      wrapperStyle={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
       wrapperClass=""
       visible={true}
     />
